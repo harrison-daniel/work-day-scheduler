c7351c0510fefd967463bb0e071073acbc8e8a9e
Harrison Daniel <hd4n@outlook.com>
2022-04-03 22:42:09 -0400

    past, rpesent, future times working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,46 @@
-var tasks = {}
+var tasks;
 
-// Display current date
-$("#currentDay").html(moment().format("dddd, MMMM Do YYYY"));
-console.log();
+// target editable task field
+var textBlock = $(".description");
+
+// get current hour
+var currentHour = moment().hour()
+
+// add cariable to set todays date at top of page
+var time = moment().format("[Today's date is] dddd, MMMM Do")
+
+// display time to top of page
+var timeDisplay = $("#currentDay");
+timeDisplay.text(time);
+
+// create timeblock variable to reference when checking
+var timeBlock = $(".hour")
+console.log(timeBlock);
+
+// check time against past, present, futuer
+var dateCheck = function () {
+    for (var i = 0; i < timeBlock.length; i++) {
+        var hour = parseInt($(timeBlock[i]).attr("id"));
+        console.log(hour)
+
+        if (hour < currentHour) {
+            $(textBlock[i]).addClass("past");
+        } else if (hour == currentHour) {
+            $(textBlock[i]).addClass("present");
+        } else {
+            $(textBlock[i]).addClass("future");
+        }
+    }
+}
+
+dateCheck();
+
+// add eventlistener to Save button
+
+// WHen Save button is clicked, grab text from text container -- save to local storage (in specific time)
+
+// load saved text into container referencing specific time upon page load
+
+
 
 
